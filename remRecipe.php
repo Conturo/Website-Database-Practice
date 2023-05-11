0794ac5c82346195bc90cb51a44a14b42075d21c
Evan <ewben@comcast.net>
2023-05-10 22:00:13 -0700

    Added removing recipe functionality

--- remRecipe.php
+++ remRecipe.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION["username"])) {
         <a href="user.php"><button class="big">Search</button></a><br><br>
         <a href="addRecipe.php"><button class="big">Add Recipe</button></a>
         <a href="remRecipe.php"><button class="big">Remove Recipe</button></a>
-        <form method="post">
+        <form method="post" action="removeRecipeScript.php">
             <br><br>
             <label for="recipeName">Recipe name:</label><br>
             <input type="text" id="recipeName" name="recipeName"><br>
@@ -28,5 +28,6 @@ if (!isset($_SESSION["username"])) {
             <input type="text" id="author" name="author"><br><br>
             <input type="submit" name="submit" value="Remove">
         </form>
+        <p style="color: red;"><?php if (isset($_SESSION["error_message"])) {echo $_SESSION["error_message"]; unset($_SESSION["error_message"]); }?></p>
     </body>
 </html>
--- removeRecipeScript.php
+++ removeRecipeScript.php
@@ -11,6 +11,31 @@ $connectionString = "host=$host port=$port dbname=$db user=$user password=$passw
 
 $connection = pg_connect($connectionString);
 
-pg_prepare($connection, "checkRecipe", "SELECT * FROM Recipe WHERE recipe_name = $1, ");
-pg_prepare($connection, "removeRecipe", "SELECT * FROM Users WHERE username = $1");
+
+
+pg_prepare($connection, "checkRecipe", "SELECT * FROM Recipes NATURAL JOIN Users WHERE recipe_name = $1 AND username = $2");
+$result = pg_execute($connection, "checkRecipe", array($_POST["recipeName"], $_POST["author"]));
+
+
+if (pg_num_rows($result) == 1) {
+
+    $recipeId = pg_fetch_result($result, 0, 0);
+    
+    pg_prepare($connection, "removeRecipe", "DELETE FROM Recipes WHERE recipe_id = $1");
+
+    $result = pg_execute($connection, "removeRecipe", array($recipeId));
+
+    $location = $location = dirname($_SERVER["PHP_SELF"]);
+    header("Location: $location/remRecipe.php");
+
+} else {
+    $_SESSION["error_message"] = "Failed to Remove Recipe";
+    $location = $location = dirname($_SERVER["PHP_SELF"]);
+    header("Location: $location/remRecipe.php");
+}
+
+
+
+pg_close($connection);
+
 ?>
